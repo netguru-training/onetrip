e293309e05f9592d4bee2434f87df3a1b8c2da78
dodo121 <dszromik@getso.pl>
2015-01-25 12:26:06 +0100

    trip_category validations update

--- app/models/trip_category.rb
+++ app/models/trip_category.rb
@@ -2,8 +2,8 @@ class TripCategory < ActiveRecord::Base
   belongs_to :trip
   belongs_to :category
 
-  validates_presence_of :trip_id, :category_id
-  
+  validates_presence_of  :category_id
+  validates :trip_id, presence: true, on: :update
   validates_uniqueness_of :category_id, scope: :trip_id
 
 end
