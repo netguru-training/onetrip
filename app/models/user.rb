ee9dcd31e76cabb3d6878f76593d85f990abc368
dodo121 <dszromik@getso.pl>
2015-01-25 11:20:37 +0100

    Facebook auth fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,12 +14,22 @@ class User < ActiveRecord::Base
   has_many :trip_memberships
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.name   # assuming the user model has a name
-      user.image = auth.info.image # assuming the user model has an image
+    return_user = self.where(email: auth.info.email).first
+    if return_user.present?
+      return_user.provider = auth.provider
+      return_user.uid = auth.uid
+    else
+      reutrn_user = self.create do |user|
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.name = auth.info.name
+        user.email = auth.info.email
+        user.password = Devise.friendly_token[0,20]
+        user.name = auth.info.name   # assuming the user model has a name
+        user.image = auth.info.image # assuming the user model has an image
+      end
     end
+    return_user
   end
   
   def self.new_with_session(params, session)
